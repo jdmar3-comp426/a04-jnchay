570e18be8faf4c2afe611c62963ab95d959b1e0c
Jason <jason.chay@yahoo.com>
2021-11-27 23:54:05 -0500

    working on post

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/ ****************************************
 app.post("/app/new/", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
-	const info = stmt.run();
+	const info = stmt.run(req.body.user, req.body.pass);
 	res.status(201).json(info.changes);
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
